7ca900e1f731fbeb43fb7b8443da30d34d01298c
smanuyag <smanuyag8554@sdsu.edu>
2025-10-30 10:30:17 -0700

    pop

--- heap.h
+++ heap.h
@@ -16,12 +16,24 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
-    }
 
+
+    }
+    //uses an int var that contains the smallest index for returning, and another with the same
+    //value to get replaced by the last element, then downheaps to maintain minheap structure
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
+        if (size == 0) {
+            cout << "Heap is empty." << endl;
+            return -1;
+        }
+        int smallest = weightArr[0];
+        int replace = weightArr[0];
+
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        replace = weightArr[size - 1];
+        //downheap();
+        return smallest;
     }
 
     void upheap(int pos, int weightArr[]) {
